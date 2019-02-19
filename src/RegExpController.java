392a5be36b8369b5938113f9dba59e952a7cc40e
dariasmetek <47626013+dariasmetek@users.noreply.github.com>
2019-02-19 15:54:30 +0100

    Add files via upload

--- src/RegExpController.java
+++ src/RegExpController.java
@@ -14,6 +14,12 @@ public class RegExpController {
         String template = "\\d{11}";
         return Pattern.matches(template, pesel);
     }
+    //+[dd][ddd] -ddd-ddd-ddd lub 321-321-321
+    public boolean checkPhoneNo(String phone_no) {
+        String template1 = "^[+]{1}\\d{2,3}-\\d{3}-\\d{3}-\\d{3}";
+        String template2 = "\\d{3}-\\d{3}-\\d{3}";
+        return Pattern.matches(template1 + "|" + template2, phone_no);
+    }
 
     public static void main(String[] args) {
         RegExpController rec = new RegExpController();
@@ -23,5 +29,8 @@ public class RegExpController {
         System.out.println(rec.checkPostalCode(" 03-032"));
         System.out.println(rec.checkPesel("81072506104"));
         System.out.println(rec.checkPesel("81d72506104"));
+        System.out.println("phone");
+        System.out.println(rec.checkPhoneNo("+48-534-528-162"));
+        System.out.println(rec.checkPhoneNo("534-528-162"));
     }
 }
